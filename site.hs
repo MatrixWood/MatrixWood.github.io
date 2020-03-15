c5ab2535377a3aff0eec8435427e9fe18d7d4a4a
CanftIn <wwc7033@gmail.com>
2020-03-15 16:11:23 +0800

    update

--- site.hs
+++ site.hs
@@ -2,6 +2,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid ((<>))
 import           Hakyll
+import           Data.List (isSuffixOf)
+
+--------------------------------------------------------------------------------
+
+siteHost :: String
+siteHost = "http:://matrixwood.netlify.com"
+
+siteName :: String
+siteName = "matrixwood"
+
+hakyllConfig :: Configuration
+hakyllConfig = defaultConfiguration
+          { destinationDirectory = "public"
+          , previewHost          = "0.0.0.0"
+          , previewPort          = 4000
+          , deployCommand        = "stack exec page build; echo \"\n\n *** add rsync command ***\""
+          }
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
+    { feedTitle = siteName
+    , feedDescription = "RSS feed for matrixwood."
+    , feedAuthorName = "wwc7033@gmail.com CanftIn"
+    , feedAuthorEmail = "wwc7033@gmail.com"
+    , feedRoot = "http:://matrixwood.netlify.com"
+    }
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -50,6 +76,15 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" writingCtx
                 >>= relativizeUrls
 
+    create ["rss.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend` bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<<
+              loadAllSnapshots "posts/*" "content"
+            renderRss feedConfiguration feedCtx posts
+              >>= cleanIndexHtmls
+
     match "index.html" $ do
         route idRoute
         compile $ do
@@ -75,3 +110,10 @@ postCtx =
     dateField "date" "%B %e, %Y" <>
     teaserField "teaser" "content" <>
     defaultContext
+
+
+cleanIndexHtmls :: Item String -> Compiler (Item String)
+cleanIndexHtmls = return . fmap (replaceAll pattern replacement)
+  where
+    pattern = "/index.html"
+    replacement = const "/"
